d3bdd46d1b83357898fb5508d3c977377228a72d
Aaron Horler <aghorler@users.noreply.github.com>
2017-09-09 15:17:23 +1000

    Add custom scripts

---
+++ scripts/deploy.sh
@@ -0,0 +1,23 @@
+#! /bin/bash
+
+cd /var/www/
+sudo rm -rf /var/www/employ.jobsaustralia.tech/
+git clone https://github.com/jobsaustralia/employ.jobsaustralia.tech.git
+cd employ.jobsaustralia.tech/
+composer install
+chmod -R 777 storage/
+cp /var/www/.env .env
+php artisan cache:clear
+composer dump-autoload
+php artisan migrate --force
+
+cd /var/www/
+sudo rm -rf /var/www/html/
+git clone https://github.com/jobsaustralia/jobsaustralia.tech.git html
+cd html/
+composer install
+chmod -R 777 storage/
+cp /var/www/.env .env
+php artisan cache:clear
+composer dump-autoload
+php artisan migrate --force --seed
