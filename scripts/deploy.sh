cf8b1187b63f4b9532bf1ec49f4e0d8260f3afca
Aaron Horler <aghorler@users.noreply.github.com>
2017-10-05 20:48:19 +1100

    Deploy Laravel 5.5 branch to server.

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,22 +2,22 @@
 
 cd /var/www/
 sudo rm -rf /var/www/employ.jobsaustralia.tech/
-git clone https://github.com/jobsaustralia/employ.jobsaustralia.tech.git
+git clone -b laravel-5.5 https://github.com/jobsaustralia/employ.jobsaustralia.tech.git
 cd employ.jobsaustralia.tech/
 composer install
 chmod -R 777 storage/
-cp /var/www/.env .env
+cp /var/www/employers.env .env
 php artisan cache:clear
 composer dump-autoload
 
 
 cd /var/www/
 sudo rm -rf /var/www/html/
-git clone https://github.com/jobsaustralia/jobsaustralia.tech.git html
+git clone -b laravel-5.5 https://github.com/jobsaustralia/jobsaustralia.tech.git html
 cd html/
 composer install
 chmod -R 777 storage/
-cp /var/www/.env .env
+cp /var/www/jobseekers.env .env
 php artisan cache:clear
 composer dump-autoload
 php artisan migrate --force --seed
